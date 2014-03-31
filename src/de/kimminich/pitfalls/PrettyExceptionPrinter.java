debed4690b10fceec6ff05fcacc240f5951f3bd7
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-03-31 11:25:41 +0200

    source layouting

--- src/de/kimminich/pitfalls/PrettyExceptionPrinter.java
+++ src/de/kimminich/pitfalls/PrettyExceptionPrinter.java
@@ -7,11 +7,12 @@ public class PrettyExceptionPrinter {
     public void prettyPrint(Throwable t, Logger logger) {
         String className = t.getClass().getName();
         String message = t.getMessage();
-        logger.severe(indefiniteArticleFor(className) + className + " has occurred: " + message != null ? message : "No message");
+        logger.severe(indefiniteArticleFor(className) + " " + className + " has occurred: " +
+                        message != null ? message : "No message");
     }
 
     private String indefiniteArticleFor(String className) {
-        return "A" + (className.matches("[AEIOU].*") ? "n " : " ");
+        return "A" + (className.matches("[AEIOU].*") ? "n" : "");
     }
 
 }
