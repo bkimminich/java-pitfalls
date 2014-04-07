d7b66cea1f3c5b518cf90e401ac949a5b9981709
Björn Kimminich <bjoern.kimminich@kuehne-nagel.com>
2014-04-07 11:02:14 +0200

    added another null check switched to using ValueObject

--- src/de/kimminich/pitfalls/NotSoNullsafeUtils.java
+++ src/de/kimminich/pitfalls/NotSoNullsafeUtils.java
@@ -1,9 +1,24 @@
 package de.kimminich.pitfalls;
 
+import de.kimminich.ValueObject;
+
 public class NotSoNullSafeUtils {
 
-    public static boolean nullSafeEquals(Integer x, Integer y) {
-        return (x == null && y == null) || x.equals(y);
+    public static boolean nullSafeEquals(ValueObject o1, ValueObject o2) {
+        return (o1 == null && o2 == null) || o1.equals(o2);
+    }
+
+    public int nullSafeCompare(ValueObject o1, ValueObject o2) {
+        if (o1 == null && o2 == null) {
+            return 0;
+        }
+        if (o1 != null && o1.getValue() == null) {
+            return -1;
+        }
+        if (o2 != null && o2.getValue() == null) {
+            return 1;
+        }
+        return o1.getValue().compareTo(o2.getValue());
     }
 
 }
