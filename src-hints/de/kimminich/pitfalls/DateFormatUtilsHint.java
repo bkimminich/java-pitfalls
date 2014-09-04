29422368ff4dfc158087086b846fccdc96135914
Björn Kimminich <bjoern.kimminich@gmx.de>
2014-09-04 03:31:28 +0200

    wrapped in try-catch to keep hint run green

--- src-hints/de/kimminich/pitfalls/DateFormatUtilsHint.java
+++ src-hints/de/kimminich/pitfalls/DateFormatUtilsHint.java
@@ -13,25 +13,29 @@ import java.util.concurrent.Future;
 public class DateFormatUtilsHint {
 
     @Test
-    public void multiThreadedDateParsing() throws Exception {
-        Callable<Date> task = new Callable<Date>() {
-            DateFormatUtils dateFormatUtils = new DateFormatUtils();
+    public void multiThreadedDateParsing() {
+        try {
+            Callable<Date> task = new Callable<Date>() {
+                DateFormatUtils dateFormatUtils = new DateFormatUtils();
 
-            public Date call() throws Exception {
-                return dateFormatUtils.toDate("1 Jan 2014");
+                public Date call() throws Exception {
+                    return dateFormatUtils.toDate("1 Jan 2014");
+                }
+            };
+
+            ExecutorService exec = Executors.newFixedThreadPool(5);
+            List<Future<Date>> results = new ArrayList<>();
+
+            for (int i = 0; i < 10; i++) {
+                results.add(exec.submit(task));
             }
-        };
+            exec.shutdown();
 
-        ExecutorService exec = Executors.newFixedThreadPool(5);
-        List<Future<Date>> results = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) {
-            results.add(exec.submit(task));
-        }
-        exec.shutdown();
-
-        for (Future<Date> result : results) {
-            System.out.println(result.get());
+            for (Future<Date> result : results) {
+                System.out.println(result.get());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
